1881c1db538a25c777e03fe20765ff8ec67ee0e4
Martin Corley <Martin.Corley@ed.ac.uk>
2023-04-18 15:48:52 +0100

    dislab

--- write_data.php
+++ write_data.php
@@ -1,7 +1,7 @@
 <?php
 $json = file_get_contents('php://input');
 $obj = json_decode($json, true);
-$server_data = '/home/corleylabppls/experiment_data/SPR';
+$server_data = '/home/dislabpplsedac/experiment_data/SPR';
 $path = $server_data."/".$obj["filename"].".csv";
 if (substr(realpath(dirname($path)), 0, strlen($server_data))!=$server_data) {
     error_log("attempt to write to bad path: ".$path);
