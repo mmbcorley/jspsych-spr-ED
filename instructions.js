1eea9c0e58c89b0734e8ed39b0f3c0430d4d9d3d
aidatar <93160007+aidatar@users.noreply.github.com>
2023-04-21 15:27:06 +0100

    Prolific code updated.

--- instructions.js
+++ instructions.js
@@ -53,7 +53,7 @@ const PRE_TEST_INSTRUCTION =
 const POST_TEST_INSTRUCTION =
     "<h1>End of the experiment.</h1>"                                   +
     "<h2>Thank you for your participation.</h2>"                        +
-    `<h2><a href="https://app.prolific.co/submissions/complete?cc=XXXXXXX">` +
+    `<h2><a href="https://app.prolific.co/submissions/complete?cc=C9C9FIRD">` +
       "Click here to return to Prolific and complete the study</a></h2>";
 
 const FINISHED_NO_CONSENT = 
